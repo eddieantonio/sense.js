17a1f25b54900564770c58e71b6b44235a807866
Eddie Antonio Santos <easantos@ualberta.ca>
2018-03-24 15:34:28 +0100

    Clean up tests a bit.

--- tests.js
+++ tests.js
@@ -1,20 +1,41 @@
+/*
+ * Copyright (C) 2018  Eddie Antonio Santos <easantos@ualberta.ca>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * Simple tests for sensibility.
+ *
+ * TODO: use ava.
+ * TODO: property-based testing.
+ */
 const assert = require('assert');
 
 const sense = require('.');
 
-/**
- * Tests
- */
 
 const simple = `function hello() {
 }
 `;
 
-assert.deepEqual([
-  'function', 'IDENTIFIER', '(', ')', '{', '}'
-],
-  sense. tokenizeJavaScript(simple)
+/* Simple tokenization. */
+assert.deepEqual(
+  ['function', 'IDENTIFIER', '(', ')', '{', '}'],
+  sense.tokenizeJavaScript(simple)
 );
+
 
 console.log('Test ok!');
 /* eslint no-console: 0 */
